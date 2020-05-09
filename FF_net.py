8b6d8856b047c1b10ceaf9e00032ada54aa5453b
tobias.oberkofler <t.oberkofler@student.maastrichtuniversity.nl>
2020-05-10 01:07:29 +0200

    update

--- FF_net.py
+++ FF_net.py
@@ -1,6 +1,9 @@
 import torch.nn as nn
 import torch
 from torch.nn import functional as F
+import numpy as np
+from complexFunctions import complex_relu, complex_max_pool2d
+from complexFunctions import complex_dropout, complex_dropout2d
 
 class CoilWeight_net(nn.Module):
     def __init__(self,nodes,channels):
@@ -40,6 +43,85 @@ class FF_net(nn.Module):
         x=self.ff_block(x)
         return x
     
+class ComplexLinear(nn.Module):
+    def __init__(self, in_features, out_features):
+        super(ComplexLinear, self).__init__()
+        self.fc_r = nn.Linear(in_features, out_features,bias=False)
+        self.fc_i = nn.Linear(in_features, out_features,bias=False)
+        #IF_R=torch.Tensor([[np.real(np.exp(2j*(j)*(i)*np.pi/in_features)) for j in range(in_features)] for i in range(in_features)])
+        #IF_IMAG=torch.Tensor([[np.imag(np.exp(2j*(j)*(i)*np.pi/in_features)) for j in range(in_features)] for i in range(in_features)])
+        IF_R=torch.Tensor([[np.cos(2*(j)*(i)*np.pi/in_features)/in_features for j in range(in_features)] for i in range(in_features)]).double()
+        IF_IMAG=torch.Tensor([[np.sin(2*(j)*(i)*np.pi/in_features)/in_features for j in range(in_features)] for i in range(in_features)]).double()
+        IFB=torch.Tensor([0 for i in range(in_features)]).double()
+        with torch.no_grad():
+            self.fc_r.weight = torch.nn.Parameter(IF_R)
+            self.fc_i.weight = torch.nn.Parameter(IF_IMAG)
+            #self.fc_r.bias = torch.nn.Parameter(IFB)
+            #self.fc_i.bias = torch.nn.Parameter(IFB)
+
+    def forward(self,input_r, input_i):
+        return self.fc_r(input_r)-self.fc_i(input_i), \
+               self.fc_r(input_i)+self.fc_i(input_r)
+    
+class ComplexSequential(nn.Sequential):
+    def forward(self, input_r, input_t):
+        for module in self._modules.values():
+            input_r, input_t = module(input_r, input_t)
+        return input_r, input_t
+
+class Complex_net_ext(nn.Module):
+    def __init__(self, n_input,args):
+        super().__init__()
+        #IF=torch.Tensor([[1 if j==i else 0 for j in range(n_input)] for i in range(n_input)])
+        self.n_input=n_input
+        self.layer = ComplexLinear(n_input, n_input)
+        self.layer2 = ComplexLinear(n_input, n_input)
+        self.args=args
+        
+    def forward(self, x):
+        for i in range(1,self.n_input):
+            x_r=x[...,i,:,:]
+            xr = x_r[...,:,0]
+            xi = x_r[...,:,1]
+            out_r,out_i = self.layer(xr, xi)
+            x[...,i,:,0]=out_r
+            x[...,i,:,1]=out_i
+        for i in range(1,self.n_input):
+            y_r=x[...,:,i,:]
+            yr = y_r[...,:,0]
+            yi = y_r[...,:,1]
+            out_r,out_i = self.layer2(yr, yi)
+            x[...,:,i,0]=out_r+out_i
+        return torch.abs(x[...,:,i,0])
+
+class Complex_net(nn.Module):
+    def __init__(self, n_input):
+        super().__init__()
+        #IF=torch.Tensor([[1 if j==i else 0 for j in range(n_input)] for i in range(n_input)])
+        self.n_input=n_input
+        self.layer = ComplexLinear(n_input, n_input)
+        
+    def forward(self, x):
+        xr = x_r[...,:self.n_input]
+        xi = x_r[...,self.n_input:]
+        out_r,out_i = self.layer(xr, xi)
+        return (out_r**2+out_i**2)**(0.5)
+    
+class FTI_net(nn.Module):
+    def __init__(self, n_input):
+        super().__init__()
+        #IF=torch.Tensor([[1 if j==i else 0 for j in range(n_input)] for i in range(n_input)])
+        self.n_input=n_input
+        self.layer = ComplexLinear(n_input, n_input)
+        self.layer2 = ComplexLinear(n_input, n_input)
+        
+    def forward(self, x):
+        xr = x[...,:self.n_input]
+        xi = x[...,self.n_input:]
+        out_r,out_i = self.layer(xr, xi)
+        out_r,out_i = self.layer2(out_r,out_i)
+        return (out_r**2+out_i**2)**(0.5)
+    
 class FF_net_tanh(nn.Module):
     def __init__(self,nodes):
         super().__init__()
@@ -48,11 +130,9 @@ class FF_net_tanh(nn.Module):
         #self.fc3 = nn.Linear(nodes//2, nodes//2)
         self.ff_block=nn.Sequential(
             nn.Linear(nodes, nodes//2),
-            nn.Tanh(),
+            nn.ELU(),
             nn.Linear(nodes//2, nodes//2),
-            nn.Tanh(),
-            nn.Linear(nodes//2, nodes//2),
-            nn.Tanh(),
+            nn.ReLU(),
         )
         
     def forward(self, x):
@@ -80,6 +160,64 @@ class FF_simple_net(nn.Module):
         x=self.ffs_block(x)
         return x
     
+class ComplexDropout(nn.Module):
+    def __init__(self,p=0.5, inplace=False):
+        super(ComplexDropout,self).__init__()
+        self.p = p
+        self.inplace = inplace
+
+    def forward(self,input_r,input_i):
+        return complex_dropout(input_r,input_i,self.p,self.inplace)
+
+class ComplexDropout2d(nn.Module):
+    def __init__(self,p=0.5, inplace=False):
+        super(ComplexDropout2d,self).__init__()
+        self.p = p
+        self.inplace = inplace
+
+    def forward(self,input_r,input_i):
+        return complex_dropout2d(input_r,input_i,self.p,self.inplace)
+
+class ComplexMaxPool2d(nn.Module):
+    def __init__(self,kernel_size, stride= None, padding = 0,
+                 dilation = 1, return_indices = False, ceil_mode = False):
+        super(ComplexMaxPool2d,self).__init__()
+        self.kernel_size = kernel_size
+        self.stride = stride
+        self.padding = padding
+        self.dilation = dilation
+        self.ceil_mode = ceil_mode
+        self.return_indices = return_indices
+
+    def forward(self,input_r,input_i):
+        return complex_max_pool2d(input_r,input_i,kernel_size = self.kernel_size,stride = self.stride, padding = self.padding,dilation = self.dilation, ceil_mode = self.ceil_mode,return_indices = self.return_indices)
+
+class ComplexReLU(nn.Module):
+     def forward(self,input_r,input_i):
+            return complex_relu(input_r,input_i)
+
+class ComplexConvTranspose2d(nn.Module):
+    def __init__(self,in_channels, out_channels, kernel_size, stride=1, padding=0,
+                 output_padding=0, groups=1, bias=True, dilation=1, padding_mode='zeros'):
+        super(ComplexConvTranspose2d, self).__init__()
+        self.conv_tran_r = ConvTranspose2d(in_channels, out_channels, kernel_size, stride, padding,output_padding, groups, bias, dilation, padding_mode)
+        self.conv_tran_i = ConvTranspose2d(in_channels, out_channels, kernel_size, stride, padding,output_padding, groups, bias, dilation, padding_mode)
+
+    def forward(self,input_r,input_i):
+        return self.conv_tran_r(input_r)-self.conv_tran_i(input_i), \
+               self.conv_tran_r(input_i)+self.conv_tran_i(input_r)
+
+class ComplexConv2d(nn.Module):
+    def __init__(self,in_channels, out_channels, kernel_size=3, stride=1, padding = 0,
+                 dilation=1, groups=1, bias=True):
+        super(ComplexConv2d, self).__init__()
+        self.conv_r = Conv2d(in_channels, out_channels, kernel_size, stride, padding, dilation, groups, bias)
+        self.conv_i = Conv2d(in_channels, out_channels, kernel_size, stride, padding, dilation, groups, bias)
+    def forward(self,input_r, input_i):
+#        assert(input_r.size() == input_i.size())
+        return self.conv_r(input_r)-self.conv_i(input_i), \
+               self.conv_r(input_i)+self.conv_i(input_r)
+
 class Combiner(nn.Module):
     def __init__(self,input_nodes):
         super(Combiner, self).__init__()
