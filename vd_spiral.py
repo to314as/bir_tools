86ee9d0d50c0b873c6116e187d94ca5a99090bb5
tobias.oberkofler <t.oberkofler@student.maastrichtuniversity.nl>
2020-06-23 18:06:24 +0200

    update

--- vd_spiral.py
+++ vd_spiral.py
@@ -1,4 +1,6 @@
 import numpy as np
+import math
+
 
 def makeSpiral(N,nRounds,PowCoeff,phiBase,m):
 #Simple VD Spiral
@@ -13,3 +15,13 @@ def makeSpiral(N,nRounds,PowCoeff,phiBase,m):
     C=r*np.exp(1j*phi)*m;
 
     return np.array([np.real(C),np.imag(C)])
+
+def PointsInCircum(r,n=100):
+    return np.array([(math.cos(2*math.pi/n*x)*r,math.sin(2*math.pi/n*x)*r) for x in range(0,n)])
+
+def makeRadial(N,spikes,s):
+#Simple Radial points
+    coords=[]
+    for i in range(1,N//spikes):
+        coords.append(PointsInCircum(s/(N//spikes)*i,n=spikes)+180)
+    return np.array(coords).reshape(-1,2)
